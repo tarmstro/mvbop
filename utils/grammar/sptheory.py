95787a208f670fbe202fc30bc18b93cc09c3a538
Tom Armstrong <tarmstro@gmail.com>
2015-01-24 22:15:09 -0500

    does python have a gensym module?

--- utils/grammar/sptheory.py
+++ utils/grammar/sptheory.py
@@ -1,14 +1,18 @@
 """
 http://arxiv.org/pdf/1306.3888.pdf
 """
+from collections import Counter
+
 
 def MK10(corpus):
-    pass
-
+    bigrams = Counter()
+    for i in xrange(len(corpus) - 1):
+        bigrams[corpus[i:i+2]] += 1
+    common, count = bigrams.most_common()[0]
 
 def SNPR(corpus):
     pass
 
 
 if __name__ == '__main__':
-    mk10('aabbccddaabbccddbbccbbccaabbccccddccdd')
+    MK10('aabbccddaabbccddbbccbbccaabbccccddccdd')
