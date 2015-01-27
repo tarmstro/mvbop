5703b2a59cfea0a1fce9023f92d1f65ac8e3430a
Tom Armstrong <tarmstro@wheatoncollege.edu>
2015-01-27 16:52:03 -0500

    Add delimiter?

--- utils/grammar/sptheory.py
+++ utils/grammar/sptheory.py
@@ -5,14 +5,19 @@ from collections import Counter
 
 
 def MK10(corpus):
+    corpus = '_'.join(corpus)
     bigrams = Counter()
-    for i in xrange(len(corpus) - 1):
-        bigrams[corpus[i:i+2]] += 1
+    for i in xrange(0, len(corpus) - 1, 2):
+        print corpus[i:i+4]
+        bigrams[corpus[i:i+4]] += 1
     common, count = bigrams.most_common()[0]
+    print common
+
 
 def SNPR(corpus):
     pass
 
 
 if __name__ == '__main__':
-    MK10('aabbccddaabbccddbbccbbccaabbccccddccdd')
+    demo = 'aabbccddaabbccddbbccbbccaabbccccddccdd'
+    MK10(demo)
