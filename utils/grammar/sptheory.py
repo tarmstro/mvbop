589133a5c51f91339792547c62482070a67fa466
Tom Armstrong <tarmstro@gmail.com>
2015-02-07 22:40:17 -0500

    moving to ipython notebook exploration

--- utils/grammar/sptheory.py
+++ utils/grammar/sptheory.py
@@ -9,6 +9,9 @@ def MK10(corpus):
     for i in xrange(len(corpus) - 1):
         bigrams[corpus[i:i+2]] += 1
     common, count = bigrams.most_common()[0]
+    print common
+    print count
+
 
 def SNPR(corpus):
     pass
